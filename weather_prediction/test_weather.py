a218b6955a857502313e6497e700d451a876a505
102sgbscc1546 <jerryhuang0312@gmail.com>
2018-04-11 11:09:14 +0800

    Add files via upload

---
+++ weather_prediction/test_weather.py
@@ -0,0 +1,133 @@
+from datetime import datetime, timedelta  
+import time  
+from collections import namedtuple  
+import pandas as pd  
+import requests  
+import matplotlib.pyplot as plt
+import pickle
+
+API_KEY = '7052ad35e3c73564'
+
+BASE_URL = "http://api.wunderground.com/api/{}/history_{}/q/NE/Lincoln.json"
+
+target_date = datetime(2018, 4, 1)  
+features = ["date", "meantempm", "meandewptm", "meanpressurem", "maxhumidity", "minhumidity", "maxtempm",  
+            "mintempm", "maxdewptm", "mindewptm", "maxpressurem", "minpressurem", "precipm"]
+DailySummary = namedtuple("DailySummary", features)
+
+def extract_weather_data(url, api_key, target_date, days):  
+    records = []
+    for _ in range(days):
+        request = BASE_URL.format(API_KEY, target_date.strftime('%Y%m%d'))
+        response = requests.get(request)
+        if response.status_code == 200:
+        	# print(response.json()['history']['dailysummary'])
+        	data = response.json()['history']['dailysummary'][0]
+        	records.append(DailySummary(
+        		date=target_date,
+        		meantempm=data['meantempm'],
+        		meandewptm=data['meandewptm'],
+        		meanpressurem=data['meanpressurem'],
+        		maxhumidity=data['maxhumidity'],
+        		minhumidity=data['minhumidity'],
+        		maxtempm=data['maxtempm'],
+        		mintempm=data['mintempm'],
+        		maxdewptm=data['maxdewptm'],
+        		mindewptm=data['mindewptm'],
+        		maxpressurem=data['maxpressurem'],
+        		minpressurem=data['minpressurem'],
+        		precipm=data['precipm']))
+        time.sleep(6)
+        target_date += timedelta(days=1)
+    return records
+
+
+# records = extract_weather_data(BASE_URL, API_KEY, target_date, 5)
+# print(records)
+
+# with open('data.txt', 'wb') as outfile:  
+#     pickle.dump(records,outfile)
+
+
+with open('data.txt', 'rb') as outfile:
+    records = pickle.load(outfile)
+ 
+
+df = pd.DataFrame(records, columns=features).set_index('date')
+
+tmp = df[['meantempm', 'meandewptm']].head(10)
+# print(tmp)
+
+def derive_nth_day_feature(df, feature, N):  
+    rows = df.shape[0]
+    nth_prior_measurements = [None]*N + [df[feature][i-N] for i in range(N, rows)]
+    col_name = "{}_{}".format(feature, N)
+    df[col_name] = nth_prior_measurements
+
+
+for feature in features:  
+    if feature != 'date':
+        for N in range(1, 4):
+            derive_nth_day_feature(df, feature, N)
+
+# print(df.columns)
+
+# make list of original features without meantempm, mintempm, and maxtempm
+to_remove = [feature  
+             for feature in features 
+             if feature not in ['meantempm', 'mintempm', 'maxtempm']]
+
+# make a list of columns to keep
+to_keep = [col for col in df.columns if col not in to_remove]
+
+# select only the columns in to_keep and assign to df
+df = df[to_keep]  
+# print(df.columns)
+# print(df.info())
+df = df.apply(pd.to_numeric, errors='coerce')  
+# print(df.info())
+
+# Call describe on df and transpose it due to the large number of columns
+spread = df.describe().T
+# print(spread)
+
+# precalculate interquartile range for ease of use in next calculation
+IQR = spread['75%'] - spread['25%']
+# print(IQR)
+
+
+# create an outliers column which is either 3 IQRs below the first quartile or
+# 3 IQRs above the third quartile
+spread['outliers'] = (spread['min']<(spread['25%']-(3*IQR)))|\
+    (spread['max'] > (spread['75%']+3*IQR))
+
+# print(spread['outliers'])
+
+# just display the features containing extreme outliers
+# print(spread.ix[spread.outliers,])
+
+# plt.rcParams['figure.figsize'] = [14, 8]  
+# df.maxhumidity_1.hist()  
+# plt.title('Distribution of maxhumidity_1')  
+# plt.xlabel('maxhumidity_1')  
+# plt.show()
+
+
+# df.minpressurem_1.hist()  
+# plt.title('Distribution of minpressurem_1')  
+# plt.xlabel('minpressurem_1')  
+# plt.show()
+
+# print(df)
+
+# iterate over the precip columns
+for precip_col in ['precipm_1', 'precipm_2', 'precipm_3']:  
+    # create a boolean array of values representing nans
+    missing_vals = pd.isnull(df[precip_col])
+    df[precip_col][missing_vals] = 0
+
+# print(df)
+
+df = df.dropna()
+
+# print(df)
